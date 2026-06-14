d43286cd574498b374ed85ac6106b94f13a39414
sellaV <zilllqvn@gmail.com>
2026-06-15 00:19:40 +0700

    Delete Neetcode - String - Contains Duplicate.py

--- Neetcode - String - Contains Duplicate.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        check = []
-        for num in nums:
-            if num not in check:
-                check.append(num)
-            else:
-                return True
-        return False
